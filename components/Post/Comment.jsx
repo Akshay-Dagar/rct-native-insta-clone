9425c162fc34091ec373c31f5df38c9136d82ee9
Akshay Dagar <akshaydagar98@gmail.com>
2023-02-19 19:25:32 +0530

    Post screen styling

--- components/Post/Comment.jsx
+++ components/Post/Comment.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 const Comment = ({text, userId}) => {
   return (
-    <View>
-        <Text>{userId}</Text>
-        <Text>{text}</Text>
+    <View style={styles.container}>
+        <Image source={require('../../assets/favicon.png')} style={styles.profileImage}/>
+        <View style={styles.metaContainer}>
+          <Text style={{fontSize: 12, fontStyle: 'italic'}}>{userId}</Text>
+          <Text style={{fontSize: 18, paddingRight: 20}}>{text}</Text>
+        </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginVertical: 20,
+    marginRight: 80
+  },
+  metaContainer: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  profileImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+    marginRight: 20
+  }
+})
 
 export default Comment
--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Text, TextInput, FlatList, ScrollView, Image } from 'react-native'
+import { Button, Text, TextInput, FlatList, ScrollView, Image, StyleSheet, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import api from '../../api'
 import Comment from './Comment'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Post = ({route}) => {
+  const post = route.params.post
+
   const [comment, setComment] = useState("")
+  const [likes, setLikes] = useState(post.likes)
   const comments = useSelector(state => state.comments.value)
   const user = useSelector(state => state.user.value)
   const dispatch = useDispatch()
 
-  const post = route.params.post
 
+  const likePost = () => {
+    setLikes(likes+1)
+    dispatch(api.likePost(post._id))
+  }
 
   useEffect(() => {
     dispatch(api.getComments(post._id))
@@ -25,21 +31,79 @@ const Post = ({route}) => {
 
   return (
     <ScrollView>
-        {/* <Image source={{uri: post.image}}/> */}
-        <Text>{post.caption}</Text>
-        <Text><Icon name="heart-outline" size={40} color={'red'}/></Text>
-        <TextInput placeholder='Add Comment...' onChangeText={txt => setComment(txt)}/>
-        <Button title='Add' onPress={addComment} disabled={comment === ""}/>
-        <FlatList 
-            numColumns={1}
-            horizontal={false}
-            data={comments}
-            renderItem={({item}) => (
-                <Comment text={item.text} userId={item.userId}/>
-            )}
-        />
+        <View style={styles.postContainer}>
+          <Image source={{uri: post.image}} style={styles.image}/>
+          <View style={styles.details}>
+            <Image source={require('../../assets/favicon.png')} style={styles.profileImage}/>
+            <Text>{post.caption}</Text>
+            <View style={styles.likes}>
+              <Icon name="heart-outline" size={25} onPress={likePost}/>
+              <Text style={styles.likeCount}>{likes}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.commentsContainer}>
+          <Text style={{borderColor: 'rgba(0, 0, 0, 0.08)', borderTopWidth: 1, marginVertical: 10}}></Text>
+          <Text style={{fontSize: 30, marginLeft: 10}}>Comments</Text>
+          <View style={styles.addCommentContainer}>
+            <TextInput placeholder='Add Comment...' onChangeText={txt => setComment(txt)} multiline style={styles.commentInputBox}/>
+            <Button title='Add' onPress={addComment} disabled={comment === ""}/>
+          </View>
+          <FlatList 
+              numColumns={1}
+              horizontal={false}
+              data={comments}
+              renderItem={({item}) => (
+                <View>
+                  <Text style={{borderColor: 'rgba(0, 0, 0, 0.08)', borderTopWidth: 1, marginVertical: 10}}></Text>
+                  <Comment text={item.text} userId={item.userId}/>
+                </View>
+              )}
+          />
+        </View>
+        
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  postContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  image: {
+    height: 700,
+    width: '100%',
+    resizeMode: 'cover'
+  },
+  details: {
+    marginVertical: 30,
+    marginHorizontal: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  likes: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  likeCount: {
+    fontSize: 19,
+    marginLeft: 5
+  },
+  commentsContainer: {
+    marginVertical: 20
+  },
+  addCommentContainer: {
+    marginHorizontal: 10
+  },
+  commentInputBox: {
+    height: 100, 
+    backgroundColor: '#fcfcfc', 
+    borderRadius: 10, 
+    marginVertical: 20
+  }
+})
 
 export default Post
